584b2546c6e03d84dae811d59145d7304cdb9958
z33k <zeek@int.pl>
2021-09-12 16:38:41 +0200

    Implement first part of data uploading logic.

--- pfin.py
+++ pfin.py
@@ -10,6 +10,8 @@
 from typing import Dict, List, Tuple, Union
 
 import gspread
+from gspread_formatting import CellFormat, Color, ColorStyle, NumberFormat, Padding, TextFormat, \
+    format_cell_range, batch_updater
 from enum import Enum
 
 
@@ -33,10 +35,10 @@ DataRows = List[DataRow]
 
 
 class InputWorksheet:
-    """Wrapper for input gspread's Worksheet object for easy retrieval of the needed data.
+    """Wrapper of input gspread's Worksheet object for easy retrieval of the needed data.
     """
-    def __init__(self, worksheet: gspread.models.Worksheet) -> None:
-        self._base_ws = worksheet
+    def __init__(self, worksheet: gspread.Worksheet, verbose=False) -> None:
+        self._base_ws, self.verbose = worksheet, verbose
         self._raw_values_full = worksheet.get_all_values(
             value_render_option=ValueRenderOption.UNFORMATTED_VALUE.name)
         self._raw_values = self._raw_values_full[4:]
@@ -53,6 +55,7 @@ class InputWorksheet:
             self.colmap["what_item"],
             self.colmap["incomings"],
             self.colmap["date"],
+            self.colmap["separator"],
             self.colmap["percentage"],
             self.colmap["share"],
         ]
@@ -77,6 +80,7 @@ class InputWorksheet:
             "what_item": 21,
             "incomings": 22,
             "date": 23,
+            "separator": 24,
             "percentage": 25,
             "share": 26,
         }
@@ -98,6 +102,7 @@ class InputWorksheet:
             "what_item": key_col_nr + 3,
             "incomings": key_col_nr + 4,
             "date": key_col_nr + 5,
+            "separator": key_col_nr + 6,
             "percentage": key_col_nr + 7,
             "share": key_col_nr + 9,  # this column is added to input after calculation
         }
@@ -113,6 +118,8 @@ class InputWorksheet:
                     value = self._values[index][self.colmap["date"] - 1]
                 new_row.append(value)
             summary_values.append(new_row)
+        if self.verbose:
+            print("Replacing raw-value dates with formatted-value dates completed.")
 
         # filter out data rows irrelevant for summary
         summary_values = [row for row in summary_values
@@ -120,22 +127,30 @@ class InputWorksheet:
                           and row[self.colmap["what_category"] - 1] != "transfer"
                           and "rozliczenie" not in row[self.colmap["incomings"] - 1]
                           and row[self.colmap["percentage"] - 1]]
+        if self.verbose:
+            print("Filtering out data rows irrevelant for the summary completed.")
 
         # calculate and add 'share' data column
         for row in summary_values:
             amount = row[self.colmap["amount"] - 1]
             percentage = row[self.colmap["percentage"] - 1]
             row.append(amount * percentage / 100)
+        if self.verbose:
+            print("Calculating and adding 'share' data column completed.")
 
         # filter out data columns irrelevant for summary
         summary_values = [[v for i, v in enumerate(row, start=1)
                            if i in self.summary_col_numbers] for row in summary_values]
+        if self.verbose:
+            print("Filtering out data columns irrevelant for the summary completed.")
 
         # divide data into summary_values and parents_summary_values
         parents_summary_values = [row for row in summary_values
                                   if row[self.colmap["ordinal"] - 1].startswith("R")]
         summary_values = [row for row in summary_values
                           if not row[self.colmap["ordinal"] - 1].startswith("R")]
+        if self.verbose:
+            print("Input data retrieval completed.")
         return summary_values, parents_summary_values
 
     @property
@@ -162,3 +177,152 @@ def input_data(spreadsheet_name: str,
     ws = ss.worksheet(worksheet_name)
     ws = InputWorksheet(ws)
     return ws.summary_values, ws.parents_summary_values
+
+
+class OutputWorksheet:
+    """Wrapper of output gspread's Worksheet object for easy data upload and manipulation.
+    """
+    FIRST_DATAROW = 3
+    DATA_COL, PERCENT_COL, RESULT_COL = "H", "J", "K"
+    FIRST_PERCENT_CELL = f"{PERCENT_COL}{FIRST_DATAROW}"
+    PERCENT_FORMAT = CellFormat(
+        backgroundColor=Color(1, 1, 1),
+        backgroundColorStyle=ColorStyle(rgbColor=Color(1, 1, 1)),
+        horizontalAlignment="RIGHT",
+        hyperlinkDisplayType="PLAIN_TEXT",
+        numberFormat=NumberFormat(type="NUMBER", pattern='#,##0"%"'),
+        padding=Padding(2, 3, 2, 3),
+        textFormat=TextFormat(
+            foregroundColor=Color(red=0.0, green=0.0, blue=0.0, alpha=1.0),
+            bold=False,
+            fontFamily="Roboto Mono",
+            fontSize=8,
+            foregroundColorStyle=ColorStyle(rgbColor=Color(red=0.0, green=0.0, blue=0.0,
+                                                           alpha=1.0)),
+            italic=False,
+            strikethrough=False,
+            underline=False
+        ),
+        verticalAlignment="BOTTOM",
+        wrapStrategy="WRAP"
+    )
+    DATA_FORMAT = CellFormat(
+        backgroundColor=Color(1, 1, 1),
+        backgroundColorStyle=ColorStyle(rgbColor=Color(1, 1, 1)),
+        horizontalAlignment="RIGHT",
+        hyperlinkDisplayType="PLAIN_TEXT",
+        numberFormat=NumberFormat(type="DATE", pattern="yyyy-mm-dd"),
+        padding=Padding(2, 3, 2, 3),
+        textFormat=TextFormat(
+            foregroundColor=Color(red=0.0, green=0.0, blue=0.0, alpha=1.0),
+            bold=False,
+            fontFamily="Roboto Mono",
+            fontSize=8,
+            foregroundColorStyle=ColorStyle(rgbColor=Color(red=0.0, green=0.0, blue=0.0,
+                                                           alpha=1.0)),
+            italic=False,
+            strikethrough=False,
+            underline=False
+        ),
+        verticalAlignment="BOTTOM",
+        wrapStrategy="WRAP"
+    )
+
+    def __init__(self, worksheet: gspread.Worksheet, summary_values: DataRows,
+                 parents_summary_values: DataRows, verbose=False) -> None:
+        self._base_ws, self.verbose = worksheet, verbose
+        self._summary_values = summary_values
+        self._parents_summary_values = parents_summary_values
+
+    @property
+    def summary_values(self) -> DataRows:
+        return self._summary_values
+
+    @property
+    def parents_summary_values(self) -> DataRows:
+        return self._parents_summary_values
+
+    def _upload_values(self, values: DataRows, first_row: int) -> None:
+        if self.verbose:
+            print("Commencing summary values upload.")
+        self._base_ws.insert_rows(values, row=first_row)
+
+        # correct Percentage and Date columns formatting
+        end_range_row = first_row + len(values) - 1
+        first_percent_cell = f"{self.PERCENT_COL}{first_row}"
+        end_percent_cell = f"{self.PERCENT_COL}{end_range_row}"
+        percent_cell_range = f"{first_percent_cell}{end_percent_cell}"  # ex. "J3:J36"
+        format_cell_range(self._base_ws, percent_cell_range, self.PERCENT_FORMAT)
+        format_cell_range(self._base_ws,
+                          percent_cell_range.replace(self.PERCENT_COL, self.DATA_COL),
+                          self.DATA_FORMAT)
+        if self.verbose:
+            print("Correction of Percentage and Data formatting completed.")
+
+        # delete the empty trailing row
+        self._base_ws.delete_rows(end_range_row + 1)
+        if self.verbose:
+            print("Trailing empty row deleted.")
+
+        # update the result's cell formula
+        result_row = end_range_row + 1
+        label = f"{self.RESULT_COL}{result_row}"
+        result_cell = self._base_ws.acell(label)
+        start_range_lbl = f"{self.RESULT_COL}{first_row}"
+        end_range_lbl = f"{self.RESULT_COL}{end_range_row}"
+        result_cell.value = f"=SUM({start_range_lbl}:{end_range_lbl})"
+        self._base_ws.update_cells([result_cell],
+                                   value_input_option=ValueInputOption.USER_ENTERED.name)
+        if self.verbose:
+            print("Updating the result cell's formula completed.")
+
+    def upload_data(self) -> None:
+        self._upload_values(self._summary_values, self.FIRST_DATAROW)
+        # # 1) UPLOADING SUMMARY VALUES
+        # if self.verbose:
+        #     print("Commencing summary values upload.")
+        # self._base_ws.insert_rows(self.summary_values, row=self.FIRST_DATAROW)
+        #
+        # # correct Percentage and Date columns formatting
+        # end_range_row = self.FIRST_DATAROW + len(self.summary_values) - 1
+        # cell_range = f"{self.FIRST_PERCENT_CELL}:{self.PERCENT_COL}{end_range_row}"  # ex. "J3:J36"
+        # format_cell_range(self._base_ws, cell_range, self.PERCENT_FORMAT)
+        # format_cell_range(self._base_ws, cell_range.replace("J", "H"), self.DATA_FORMAT)
+        # if self.verbose:
+        #     print("Correction of Percentage and Data formatting completed.")
+        #
+        # # delete the empty trailing row
+        # self._base_ws.delete_rows(self.FIRST_DATAROW + len(self.summary_values))
+        # if self.verbose:
+        #     print("Trailing empty row deleted.")
+        #
+        # # update the result's cell formula
+        # result_row = end_range_row + 1
+        # label = f"{self.RESULT_COL}{result_row}"
+        # result_cell = self._base_ws.acell(label)
+        # start_range_lbl = f"{self.RESULT_COL}{self.FIRST_DATAROW}"
+        # end_range_lbl = f"{self.RESULT_COL}{end_range_row}"
+        # result_cell.value = f"=SUM({start_range_lbl}:{end_range_lbl})"
+        # self._base_ws.update_cells([result_cell],
+        #                            value_input_option=ValueInputOption.USER_ENTERED.name)
+        # if self.verbose:
+        #     print("Updating the result cell's formula completed.")
+
+        # # 2) UPLOADING PARENT'S SUMMARY VALUES
+        # first_row = result_row + 1
+        # self._base_ws.insert_rows(self.parents_summary_values, row=first_row)
+        #
+        # # correct Percentage column formatting
+        # cell_range = f"{self.PERCENT_COL}{first_row}:{self.PERCENT_COL}" \
+        #              f"{first_row + len(self.parents_summary_values) - 1}"  # ex. "J3:J36"
+        # format_cell_range(self._base_ws, self.PERCENT_FORMAT, cell_range)
+        #
+        # # delete the empty trailing row
+        # self._base_ws.delete_rows(self.FIRST_DATAROW + len(self.summary_values))
+        #
+        # # update the result's cell formula
+        # result_row = self.FIRST_DATAROW + len(self.summary_values)
+        # label = f"{self.RESULT_COL}{result_row}"
+        # result_cell = self._base_ws.acell(label)
+        # self._base_ws.update_cells([result_cell],
+        #                            value_input_option=ValueInputOption.USER_ENTERED.name)
--- run.py
+++ run.py
@@ -7,8 +7,20 @@
     @author: z33k
 
 """
-from pfin import input_data
-sv, psv = input_data("pfin_2021", "202108")
-print()
+import gspread
+from pfin import InputWorksheet, CREDS_FILE, OutputWorksheet
+
+spreadsheet_name, worksheet_name = "pfin_2021", "202108"
+gc = gspread.service_account(filename=CREDS_FILE)
+ss = gc.open(spreadsheet_name)
+ws = ss.worksheet(worksheet_name)
+ws = InputWorksheet(ws, verbose=True)
+sv, psv = ws.summary_values, ws.parents_summary_values
+
+t = ss.worksheet("template_sandbox")
+ows = OutputWorksheet(t, sv, psv, verbose=True)
+ows.upload_data()
+
+
 
 
